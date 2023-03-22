2cfeba80011e1ab709b6a601f56ecf2e63252d44
kittipong199 <68522978+kittipong199@users.noreply.github.com>
2023-03-22 22:13:23 +0700

    Create Project using Google MAP

---
+++ android/app/src/main/kotlin/com/deaf/google_map/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.deaf.google_map
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/page/signIn.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+class SignInPage extends StatefulWidget {
+  const SignInPage({super.key});
+
+  @override
+  State<SignInPage> createState() => _SignInPageState();
+}
+
+class _SignInPageState extends State<SignInPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color.fromARGB(255, 89, 8, 150),
+      body: Container(
+
+
+
+          ),
+         
+
+       
+
+     
+    );
+  }
+
+  
+}
